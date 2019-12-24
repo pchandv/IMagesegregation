18baa0739b134e1ec40bd78706f856dbaafe3516
Praveen Visam <Praveen.Visam@sbdinc.com>
2019-12-25 02:09:17 +0530

    Add project files.

---
+++ ConsumeMLLibrary/FileHelperService.cs
@@ -0,0 +1,67 @@
+﻿using ML_Practice_2ML.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace IMService
+{
+    public class FileHelperService
+    {
+        static ModelOutput ProcessImage(string path)
+        {
+            var input = new ModelInput();
+            input.ImageSource = path;
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            return result;
+        }
+
+
+        public static void SegaregateImagesBasedOnFeatureName(string parentDirectory, string predictionName, string destionationFolderPath = null)
+        {
+            List<string> listOffilePathsToMove = new List<string>();
+            Console.WriteLine("-------Processing Image--------");
+            foreach (string filePath in System.IO.Directory.GetFiles(parentDirectory, "*", SearchOption.AllDirectories))
+            {
+                Console.WriteLine(filePath);
+                var mlresult = ProcessImage(filePath);
+
+                if (mlresult.Prediction == predictionName)
+                {
+                    MoveToNewFolder(filePath, predictionName, destionationFolderPath);
+                }
+            }
+            //MoveToNewFolder(listOffilePathsToMove, predictionName);
+
+            Console.WriteLine("------Processing ends-------------");
+        }
+
+        private static void MoveToNewFolder(List<string> listOffilePathsToMove, string predicteName)
+        {
+            foreach (var filePath in listOffilePathsToMove)
+            {
+                FileInfo f = new FileInfo(filePath);
+                string d = Path.Combine(f.DirectoryName, predicteName, f.Name);
+
+                f.MoveTo(d);
+                Console.WriteLine("File Moved->" + filePath);
+            }
+        }
+
+        private static void MoveToNewFolder(string filePath, string predicteName, string destionationFolderPath = null)
+        {
+            FileInfo f = new FileInfo(filePath);
+            string destinationRootFolder = string.IsNullOrEmpty(destionationFolderPath) ?
+                Path.Combine(f.DirectoryName, predicteName) :
+                Path.Combine(destionationFolderPath, predicteName);
+            string d = Path.Combine(destinationRootFolder, f.Name);
+            if (!Directory.Exists(destinationRootFolder))
+            {
+                Directory.CreateDirectory(destinationRootFolder);
+            }
+            f.MoveTo(d);
+            Console.WriteLine("File Moved->" + filePath);
+        }
+    }
+}
---
+++ ML_Practice_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using ML_Practice_2ML.Model;
+using IMService;
+using System;
+using System.IO;
+
+namespace ML_Practice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program Starts Now");
+            Console.WriteLine("Enter Source Path");
+            string sourcePath =Console.ReadLine() ;
+            Console.WriteLine("Enter Destination Path");
+            string destionationPath = Console.ReadLine();
+            Console.WriteLine("Enter LabelName to process");
+            //Rose,Tupile
+            string predicateName = Console.ReadLine();
+            FileHelperService.SegaregateImagesBasedOnFeatureName(sourcePath,predicateName,destionationPath);
+
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
